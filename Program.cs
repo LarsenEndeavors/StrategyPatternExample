ef6e7e47619af66202fc395cf11d75b4310f2898
Nicholas Larsen <nlarsen@access-healthservices.com>
2021-10-07 09:10:38 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StrategyPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
